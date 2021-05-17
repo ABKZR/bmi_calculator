66e70f36a054ce1d9cfbce4d073b7f35c80357ec
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-05-18 02:05:11 +0500

    changes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   const InputPage({Key key}) : super(key: key);
   @override
   _InputPageState createState() => _InputPageState();
 }
+
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -15,10 +17,51 @@ class _InputPageState extends State<InputPage> {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-            "SomeThing"
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusalbeCard(color: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusalbeCard(color: Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusalbeCard(color: Color(0xFF1D1E33),),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusalbeCard(color: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusalbeCard(color: Color(0xFF1D1E33), ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusalbeCard extends StatelessWidget {
+  ReusalbeCard({@required this.color});
+    final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
